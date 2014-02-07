d94942efa190edaef0a1f9be6d05f1c0f837e15f
geek65535 <paul@paularcher.org>
2014-02-06 20:49:34 -0600

    added Perl module for reference added stub libusb code from libusb module example

--- lib/delcom/vsi.rb
+++ lib/delcom/vsi.rb
@@ -1,13 +1,30 @@
 #!/usr/bin/ruby
 #
+
 require 'libusb'
 
 module Delcom
 
 	class VSI
+		VENDORID  = 4037
+		PRODUCTID = 4643
+		#VENDORID  = 0x0fc5
+		#PRODUCTID = 0x1223
 		def self.test
 			puts "in Delcom::VSI!"
 		end
+
+		usb = LIBUSB::Context.new
+		vsi = usb.devices(:idVendor => VENDORID, :idProduct => PRODUCTID).first
+		vsi.open_interface(0) do |handle|
+			  handle.control_transfer(
+				  :bmRequestType => 0x40,
+				  :bRequest => 0xa0,
+				  :wValue => 0xe600,
+				  :wIndex => 0x0000,
+				  :dataOut => 1.chr)
+		end
+
 	end
 
 end
